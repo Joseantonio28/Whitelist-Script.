5f18b030639f0d4c8f80266d86b7711c708d35c9
Joseantonio28 <70982188+Joseantonio28@users.noreply.github.com>
2020-09-09 03:00:28 +0200

    Update default.config.lua

--- default.config.lua
+++ default.config.lua
@@ -5,4 +5,4 @@ steamIdsList = {
   'STEAMID64',  
 }
 
-errorMessage = 'Joseantonio_Whitelist: No tienes acceso a la Whitelist.'
+errorMessage = 'Joseantonio_Whitelist: No tienes acceso a la Whitelist, debes pasarla en el discord del OesteRP.'
