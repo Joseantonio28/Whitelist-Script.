4cfec0cd947d5a7c15d138d21646bc207c708363
Joseantonio28 <70982188+Joseantonio28@users.noreply.github.com>
2020-10-04 11:05:45 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -13,7 +13,7 @@ AddEventHandler('playerConnecting', function(name, setReason)
   end
 end)
 
--- Devuelve VERDADERO si el valor está en la matriz, FALSO en caso contrario
+-- Devuelve verdadero si el valor está en la matriz, Falso en caso contrario
 
 function inArray(value, array)
   for _,v in pairs(array) do
@@ -25,7 +25,7 @@ function inArray(value, array)
   return false
 end
 
--- Devuelve TRUE si steamId comienza con "steam:", FALSE en caso contrario
+-- Devuelve verdadero si steamId comienza con "steam:", falso en caso contrario
 function isNativeSteamId(steamId)
   if string.sub(steamId, 0, 6) == "steam:" then
     return true
